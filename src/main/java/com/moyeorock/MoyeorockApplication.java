8dbf587d1b39c8c470897651ff603d4503999807
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-08-04 16:59:12 +0900

    init: 초기 설정

---
+++ src/main/java/com/moyeorock/MoyeorockApplication.java
@@ -0,0 +1,13 @@
+package com.moyeorock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeorockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoyeorockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moyeorock/MoyeorockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyeorock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeorockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
